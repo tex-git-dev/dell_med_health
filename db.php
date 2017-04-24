71b8d59ddad4b1ac30a1d5e2554372206856bf85
Tyagi, Manoj <manoj.kumar@luminad.com>
2017-04-24 19:22:36 +0530

    April 24, 2014

--- db.php
+++ db.php
@@ -6,17 +6,17 @@ date_default_timezone_set("Asia/Kolkata");
 */
 ?>
 <?php  
-
+/*
 $dbcon=mysqli_connect("localhost","root","098pmg123");  
 mysqli_select_db($dbcon,"dellitl"); 
 date_default_timezone_set("Asia/Kolkata"); 
-
+*/
 ?>
 
 <?php  
-/*
+
 $dbcon=mysqli_connect("23.253.97.11","dellmedapps_2","itlrocks");  
 mysqli_select_db($dbcon,"dell_med"); 
 date_default_timezone_set("America/New York"); 
-*/
+
 ?>
