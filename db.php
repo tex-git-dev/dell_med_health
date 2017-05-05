8d4a901a227a619938b8e7adf4e11c3a7c29dff0
dwaynegooden <dwayne.gooden@adaptive-datamatics.com>
2017-05-05 16:05:10 +0000

    fixed db.php

--- db.php
+++ db.php
@@ -12,11 +12,11 @@ mysqli_select_db($dbcon,"dellitl");
 date_default_timezone_set("Asia/Kolkata"); 
 */
 ?>
-
 <?php  
 
 $dbcon=mysqli_connect("23.253.97.11","dellmedapps_2","itlrocks");  
 mysqli_select_db($dbcon,"dell_med"); 
+<<<<<<< Updated upstream
 date_default_timezone_set("America/New York"); 
 
 ?>
