8352cc5ddbe056f278a48ad36c598566d38b8a28
Tyagi, Manoj <manoj.kumar@luminad.com>
2017-05-22 13:03:07 +0530

    May 22, 2017 Final Code

--- db.php
+++ db.php
@@ -1,20 +1,22 @@
 <?php 
+require_once 'dell/important/dell.php';
 function Connect_Open()
-{ /*
+{/*
 $dbcon=mysqli_connect("localhost","root","");
 mysqli_select_db($dbcon,"dellitl");
 date_default_timezone_set("Asia/Kolkata");	
 
-
+/*
 $dbcon=mysqli_connect("localhost","root","098pmg123");  
 mysqli_select_db($dbcon,"dellitl"); 
 date_default_timezone_set("Asia/Kolkata"); 
 */
 
-$dbcon=mysqli_connect("23.253.97.11","dellmedapps_2","itlrocks");  
+$user = user();
+$pwd = pwd();
+$dbcon=mysqli_connect("23.253.97.11",$user,$pwd);  
 mysqli_select_db($dbcon,"dell_med"); 
 date_default_timezone_set("America/New York"); 
-
 return $dbcon;
 }
 
